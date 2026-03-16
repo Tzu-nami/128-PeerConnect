f9fdbcbd0a03a6025868f031bb016d7d01b64fb1
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2026-03-17 01:22:30 +0800

    chore: add swiper.js for image carousel

--- PeerConnect-app/resources/js/app.js
+++ PeerConnect-app/resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    spaceBetween: 15,
+    initialSlide: 2,
+    watchSlidesProgress: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
