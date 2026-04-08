fab32210a51d915b4c451af193fc7d6aaa4c1c83
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2026-04-08 19:29:10 +0800

    style(landing-layout): remove font declarations

--- PeerConnect-app/resources/views/components/layout/landing.blade.php
+++ PeerConnect-app/resources/views/components/layout/landing.blade.php
@@ -1,36 +1,32 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'UPB LRC - PeerConnect') }}</title>
+        <title>{{ config('app.name', 'UPB LRC - PeerConnect') }}</title>
 
-    {{-- Fonts and Icons --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet">
+        {{-- Fonts and Icons --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
+        {{-- Scripts --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
 
-    {{-- Scripts --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+    <body class="antialiased">
+        <header>
+            <livewire:welcome.navigation/>
+        </header>
 
-<body class="antialiased">
-    <header>
-        <livewire:welcome.navigation/>
-    </header>
+        <main>
+            {{ $slot }}
+        </main>
 
-    <main>
-        {{ $slot }}
-    </main>
-
-    <livewire:welcome.footer/>
-</body>
+        <footer>
+            <livewire:welcome.footer/>
+        </footer>
+    </body>
 </html>
