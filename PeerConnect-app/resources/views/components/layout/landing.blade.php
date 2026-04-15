7cba5b196e363b518bf25c06f1bc5b1a34e353c7
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2026-04-15 12:00:37 +0800

    refactor(landing): update layout to support responsive design

--- PeerConnect-app/resources/views/components/layout/landing.blade.php
+++ PeerConnect-app/resources/views/components/layout/landing.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+              content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -16,17 +16,17 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
-    <body class="antialiased">
+    <body class="antialiased flex flex-col min-h-screen">
         <header>
             <livewire:welcome.navigation/>
         </header>
 
-        <main class="{{ isset($noMargin) ? '' : 'mt-[83px]' }}">
+        <main class="{{ isset($noMargin) ? '' : 'mt-[60px] md:mt-[83px]' }}">
             {{ $slot }}
         </main>
 
         <footer>
-            <livewire:welcome.footer/>
+{{--            <livewire:welcome.footer/>--}}
         </footer>
     </body>
 </html>
