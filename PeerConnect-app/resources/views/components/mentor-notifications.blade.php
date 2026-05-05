f630c451d90103efd256b6591c1a2fa812f7e34b
DanielDyoco <dbdyoco@up.edu.ph>
2026-05-05 19:40:39 +0800

    Update mentor-notifications.blade.php

--- PeerConnect-app/resources/views/components/mentor-notifications.blade.php
+++ PeerConnect-app/resources/views/components/mentor-notifications.blade.php
@@ -252,28 +252,31 @@
                 </div>
             @endforelse
 
-            {{-- Remaining notifications (shown when showAll = true) --}}
-            @foreach($notifications->skip(3) as $notif)
-                <div class="px-4 py-3 border-b border-gray-50 hover:bg-gray-50 transition cursor-default"
-                     x-show="showAll" x-cloak x-transition>
-                    <div class="flex items-start gap-3">
-                        <div class="w-8 h-8 rounded-full {{ $notif['iconBg'] }} flex items-center justify-center flex-shrink-0 mt-0.5">
-                            <i class="fa-solid {{ $notif['icon'] }} {{ $notif['iconColor'] }} text-xs"></i>
-                        </div>
-                        <div class="flex-1 min-w-0">
-                            <p class="text-xs text-gray-700 leading-snug">{!! $notif['message'] !!}</p>
-                            <div class="flex items-center gap-2 mt-1.5">
-                                <span class="text-[10px] font-bold px-1.5 py-0.5 rounded {{ $notif['badge'] }}">
-                                    {{ $notif['badgeText'] }}
-                                </span>
-                                <span class="text-[10px] text-gray-400">{{ $notif['timeAgo'] }}</span>
+{{-- Remaining notifications (shown when showAll = true) --}}
+        <template x-if="showAll">
+            <div>
+                @foreach($notifications->skip(3) as $notif)
+                    <div class="px-4 py-3 border-b border-gray-50 hover:bg-gray-50 transition cursor-default">
+                        <div class="flex items-start gap-3">
+                            <div class="w-8 h-8 rounded-full {{ $notif['iconBg'] }} flex items-center justify-center flex-shrink-0 mt-0.5">
+                                <i class="fa-solid {{ $notif['icon'] }} {{ $notif['iconColor'] }} text-xs"></i>
+                            </div>
+                            <div class="flex-1 min-w-0">
+                                <p class="text-xs text-gray-700 leading-snug">{!! $notif['message'] !!}</p>
+                                <div class="flex items-center gap-2 mt-1.5">
+                                    <span class="text-[10px] font-bold px-1.5 py-0.5 rounded {{ $notif['badge'] }}">
+                                        {{ $notif['badgeText'] }}
+                                    </span>
+                                    <span class="text-[10px] text-gray-400">{{ $notif['timeAgo'] }}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
+        </template>
 
-        </div>
+        </div>{{-- end Notification List --}}
 
         {{-- Footer --}}
         @if($notifications->count() > 3)
@@ -285,5 +288,5 @@
             </div>
         @endif
 
-    </div>
-</div>
+    </div>{{-- end Dropdown --}}
+</div>{{-- end relative wrapper --}}
