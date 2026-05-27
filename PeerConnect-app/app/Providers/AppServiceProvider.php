2cbbd7c66596bfd876b792a66508bad6200464c1
TzuNami <friankarl05@gmail.com>
2026-05-27 23:55:06 +0800

    Update AppServiceProvider.php

--- PeerConnect-app/app/Providers/AppServiceProvider.php
+++ PeerConnect-app/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Services\Avatar;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+            URL::forceScheme('https');
+        }
         // Allow variables to be available in all views
         View::composer('*', function ($view) {
             $user = auth()->user();
